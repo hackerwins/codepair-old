e763a26844ea2db65be2150578d7015f1c768708
hackerwins <susukang98@gmail.com>
2020-09-02 17:17:15 +0900

    Add example by following post using Redux and Hooks
    https://levelup.gitconnected.com/react-functional-components-managing-state-with-redux-and-hooks-a372b1a77514
    https://create-react-app.dev/docs/setting-up-your-editor/#formatting-code-automatically

---
+++ src/store/store.ts
@@ -0,0 +1,29 @@
+/*  Imports from Redux:
+ applyMiddleware: Applies middleware to the dispatch method of the Redux store
+ combineReducers: Merges reducers into one
+ createStore: Creates a Redux store that holds the state tree
+ Store: The TS Type used for the store, or state tree
+ */
+import { applyMiddleware, combineReducers, createStore, Store } from 'redux';
+/*  Thunk
+Redux Thunk middleware allows you to write action creators that return a function instead of an action. The thunk can be used to delay the dispatch of an action, or to dispatch only if a certain condition is met. The inner function receives the store methods dispatch and getState as parameters.
+*/
+import thunk from 'redux-thunk';
+// Import reducers and state type
+import { IDogState, dogReducer } from '../reducers/dogReducer';
+
+// Create an interface for the application state
+export interface IAppState {
+  dogState: IDogState;
+}
+
+// Create the root reducer
+const rootReducer = combineReducers<IAppState>({
+  dogState: dogReducer,
+});
+
+// Create a configure store function of type `IAppState`
+export default function configureStore(): Store<IAppState, any> {
+  const store = createStore(rootReducer, undefined, applyMiddleware(thunk));
+  return store;
+}
