6a271d51fa4f7502fde9652c457d00305951e010
hackerwins <susukang98@gmail.com>
2020-10-05 13:56:35 +0900

    Add `deploy` command to publish to gh-pages
    Use HashRouter to avoid 404 error on browser refresh in gh-pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Route, Redirect } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import CssBaseline from '@material-ui/core/CssBaseline';
