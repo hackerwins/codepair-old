d596a6b635673e0a7b55222d5e0ba55a35205340
hyunwoo.jo <hyunwoo.jo@navercorp.com>
2020-11-29 00:03:21 +0900

    Add material theme override on icon common css

--- src/theme.tsx
+++ src/theme.tsx
@@ -5,6 +5,18 @@ const theme = createMuiTheme({
   palette: {
     type: 'dark',
   },
+  overrides: {
+    MuiSvgIcon: {
+      root: {
+        cursor: 'pointer',
+        opacity: 0.8,
+        transition: '0.3s',
+        '&:hover': {
+          opacity: 1,
+        },
+      },
+    },
+  },
 });
 
 export default theme;
