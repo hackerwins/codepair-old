046e16b5659a8b96f9cb98f5c7a7b8c7e0ef1a22
hyunwoo.jo <hyunwoo.jo@navercorp.com>
2020-12-01 00:24:02 +0900

    Add common model component

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import MaterialModal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    modal: {
+      position: 'fixed',
+      top: '50%',
+      left: '50%',
+      transform: `translate(-50%, -50%)`,
+    },
+  }),
+);
+
+type ModalProps = {
+  open: boolean;
+  onClose: (event: object, reason: string) => void;
+  children: any;
+};
+
+/**
+ * @param {ModalProps} props
+ */
+const Modal = (props: ModalProps) => {
+  const { open, onClose, children } = props;
+  const classes = useStyles();
+
+  return (
+    <MaterialModal
+      open={open}
+      onClose={onClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <div className={classes.modal}>{children}</div>
+    </MaterialModal>
+  );
+};
+
+export default Modal;
