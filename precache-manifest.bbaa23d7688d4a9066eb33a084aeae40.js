2ec3f8813b366652bbdc82041369750462461f7f
hackerwins <susukang98@gmail.com>
2021-01-16 19:38:07 +0900

    Updates

---
+++ precache-manifest.bbaa23d7688d4a9066eb33a084aeae40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "418e6bac8c5886394001ab9a80d994dd",
+    "url": "/yorkie-codepair/index.html"
+  },
+  {
+    "revision": "a7341f449577379a8d1d",
+    "url": "/yorkie-codepair/static/css/2.b2f02b1f.chunk.css"
+  },
+  {
+    "revision": "fe64ab371cf8a839abf8",
+    "url": "/yorkie-codepair/static/css/main.ffbdb564.chunk.css"
+  },
+  {
+    "revision": "a7341f449577379a8d1d",
+    "url": "/yorkie-codepair/static/js/2.0c09025d.chunk.js"
+  },
+  {
+    "revision": "08cb4a1ede7e861973528b3691298303",
+    "url": "/yorkie-codepair/static/js/2.0c09025d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe64ab371cf8a839abf8",
+    "url": "/yorkie-codepair/static/js/main.0e2d01d4.chunk.js"
+  },
+  {
+    "revision": "1516b82bdb669ea6d86c",
+    "url": "/yorkie-codepair/static/js/runtime-main.a1586e49.js"
+  }
+]);
