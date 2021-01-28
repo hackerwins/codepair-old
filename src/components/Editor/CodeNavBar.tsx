15d573e0585228c196790b48c61d601ab470bc86
Jo Hyun Woo <hyunwoo.jo@navercorp.com>
2021-01-28 11:38:19 +0900

    Add CodeNavBar to change code mode (#33)
    Co-authored-by: Youngteac Hong <susukang98@gmail.com>

---
+++ src/components/Editor/CodeNavBar.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import Grid from '@material-ui/core/Grid';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+import { AppState } from 'reducers/rootReducer';
+import { CodeMode, setCodeMode } from 'reducers/settingReducer';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      backgroundColor: theme.palette.grey['800'],
+      padding: '6px',
+      position: 'relative',
+    },
+    formControl: {
+      minWidth: 140,
+      backgroundColor: 'transparent',
+      border: `1px solid ${theme.palette.grey['800']}`,
+      borderRadius: '4px',
+    },
+    selectEmpty: {
+      paddingLeft: '12px',
+    },
+    box: {
+      display: 'flex',
+      justifyContent: 'space-between',
+    },
+  }),
+);
+
+export default function CodeNavBar() {
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
+  const menu = useSelector((state: AppState) => state.settingState.menu);
+
+  const handleCodeModeChange = (event: React.ChangeEvent<{ name?: string; value: unknown }>) => {
+    dispatch(setCodeMode(event.target.value as CodeMode));
+  };
+
+  return (
+    <div className={classes.root}>
+      <Grid container direction="row" justify="space-between" alignItems="flex-end">
+        <Grid item>
+          <FormControl className={classes.formControl}>
+            <Select
+              name="codeMode"
+              value={menu.codeMode}
+              onChange={handleCodeModeChange}
+              className={classes.selectEmpty}
+              disableUnderline
+              displayEmpty
+            >
+              {Object.entries(CodeMode).map(([display, mode]: [string, string]) => {
+                return (
+                  <MenuItem value={mode} key={mode}>
+                    {display}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
