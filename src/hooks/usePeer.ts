b6bfd335be90db554812756a18e007f24444394d
Hyunwoo.jo <xodltus@naver.com>
2021-02-03 01:44:37 +0900

    Apply usePeer cache

--- src/hooks/usePeer.ts
+++ src/hooks/usePeer.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/indent */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
 import { AppState } from 'app/rootReducer';
@@ -9,16 +10,18 @@ export default function usePeer() {
   const client = useSelector((state: AppState) => state.docState.client);
   const peers = useSelector((state: AppState) => state.peerState.peers);
 
-  const activePeers = client
-    ? Object.entries(peers)
-        .filter(([clientId, clientInfo]) => {
-          if (client.getID() === clientId) {
-            return false;
-          }
-          return clientInfo.status === ConnectionStatus.Connected;
-        })
-        .map(([_, clientInfo]) => clientInfo)
-    : [];
+  const activePeers = useMemo(() => {
+    return client
+      ? Object.entries(peers)
+          .filter(([clientId, clientInfo]) => {
+            if (client.getID() === clientId) {
+              return false;
+            }
+            return clientInfo.status === ConnectionStatus.Connected;
+          })
+          .map(([_, clientInfo]) => clientInfo)
+      : [];
+  }, [client, peers]);
 
   return { activePeers };
 }
