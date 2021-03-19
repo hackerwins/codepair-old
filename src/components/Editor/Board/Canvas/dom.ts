ec9bf967f9513b8f5832ac3c80cc930372aa7ba3
Hyunwoo.Jo <hyunwoo.jo@navercorp.com>
2021-03-19 22:46:41 +0900

    Support Mobile device on drawing board (#70)

---
+++ src/components/Editor/Board/Canvas/dom.ts
@@ -0,0 +1,44 @@
+export function addEvent(el: EventTarget, type: string, fn: EventListener, capturing?: boolean) {
+  return el.addEventListener(type, fn, capturing);
+}
+
+export function removeEvent(el: EventTarget, type: string, fn: EventListener, capturing?: boolean) {
+  return el.removeEventListener(type, fn, capturing);
+}
+
+const touch = {
+  mouseup: 'touchend',
+  mouseout: 'touchend',
+  mousedown: 'touchstart',
+  mousemove: 'touchmove',
+};
+
+const pointers = {
+  mouseup: 'pointerup',
+  mouseout: 'pointerup',
+  mousedown: 'pointerdown',
+  mousemove: 'pointermove',
+};
+
+const microsoft = {
+  mouseup: 'MSPointerUp',
+  mouseout: 'MSPointerUp',
+  mousedown: 'MSPointerDown',
+  mousemove: 'MSPointerMove',
+};
+
+type MouseType = 'mouseup' | 'mouseout' | 'mousedown' | 'mousemove';
+
+/**
+ * {@linkcode https://github.com/bevacqua/dragula/blob/e0bcdc72ae8e0b85e17f154957bdd0cc2e2e35db/dragula.js#L498}
+ */
+export function touchy(el: EventTarget, event: Function, type: MouseType, fn: EventListener) {
+  if (global.navigator.pointerEnabled) {
+    event(el, pointers[type], fn);
+  } else if (global.navigator.msPointerEnabled) {
+    event(el, microsoft[type], fn);
+  } else {
+    event(el, touch[type], fn);
+    event(el, type, fn);
+  }
+}
