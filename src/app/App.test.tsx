7f6b7bef430db132c7eff7205e402f95fa322bec
Hyunwoo.jo <hyunwoo.jo@navercorp.com>
2021-04-13 01:25:18 +0900

    Add utils testcase

--- src/app/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders get button', () => {
-  const { getByText } = render(<App />);
-  const titleElement = getByText(/Yorkie CodePair/i);
-  expect(titleElement).toBeInTheDocument();
-});
