d0762a5c8077be56e432f91042717bfd7851e371
Hackerwins <susukang98@gmail.com>
2021-11-06 14:20:08 +0900

    Add React Router Integration

--- webapp/src/app/App.tsx
+++ webapp/src/app/App.tsx
@@ -1,6 +1,9 @@
 import React, { useMemo, useCallback } from 'react';
 import { useSelector } from 'react-redux';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { Router, Route, Redirect, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { AppState } from 'app/rootReducer';
@@ -8,6 +11,23 @@ import { Theme } from 'features/settingSlices';
 
 import DocPage from 'pages/DocPage';
 import './App.scss';
+
+const history = createBrowserHistory();
+
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://d287d6df8c6f423189266360055e6ca7@o553194.ingest.sentry.io/5680102',
+    release: `yorkie-codepair@${process.env.REACT_APP_GIT_HASH}`,
+    integrations: [new Integrations.BrowserTracing({
+      routingInstrumentation: Sentry.reactRouterV5Instrumentation(history),
+    })],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
 
 function App() {
   const menu = useSelector((state: AppState) => state.settingState.menu);
@@ -35,9 +55,11 @@ function App() {
     <ThemeProvider theme={theme}>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
       <CssBaseline />
-      <Router>
-        <Route path="/" exact render={handleRender} />
-        <Route path="/:docKey" exact component={DocPage} />
+      <Router history={history}>
+        <Switch>
+          <Route path="/" exact render={handleRender} />
+          <Route path="/:docKey" exact component={DocPage} />
+        </Switch>
       </Router>
     </ThemeProvider>
   );
--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import ReactGA from 'react-ga';
-import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
 import App from 'app/App';
 import store from 'app/store';
 
@@ -11,19 +9,6 @@ import * as serviceWorker from 'serviceWorker';
 import 'scss/global.scss';
 
 ReactGA.initialize('UA-42438082-5');
-
-if (process.env.NODE_ENV === 'production') {
-  Sentry.init({
-    dsn: 'https://d287d6df8c6f423189266360055e6ca7@o553194.ingest.sentry.io/5680102',
-    release: `yorkie-codepair@${process.env.REACT_APP_GIT_HASH}`,
-    integrations: [new Integrations.BrowserTracing()],
-
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
-  });
-}
 
 ReactDOM.render(
   <Provider store={store}>
