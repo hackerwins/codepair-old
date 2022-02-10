db7ab684d92541a230cb4f23c5ac31a1822ef53b
Eddie <40482274+eddie0329@users.noreply.github.com>
2022-02-10 16:21:40 +0900

    Working in progress delete tab

--- webapp/src/components/Editor/EditorTabNav/EditorTab.tsx
+++ webapp/src/components/Editor/EditorTabNav/EditorTab.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
+/* eslint-disable no-param-reassign */
+import React, { MouseEvent } from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
+// import { selectDoc } from 'features/docSlices';
+// import { useSelector } from 'react-redux';
+import { Link, useHistory } from 'react-router-dom';
 
 interface EditorTabProps {
   isSelected: boolean;
   index: number;
 }
 
-export default function EditorTab({ isSelected, index }: EditorTabProps){
+export default function EditorTab({ isSelected, index }: EditorTabProps) {
+  const history = useHistory();
+  // const doc = useSelector(selectDoc);
+
+  // TODO: Need deletion
+  const deleteTab = (e: MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    e.preventDefault();
+    history.push({ search: `?tab=${index - 1}` });
+  };
+
   return (
-    <li className={`editor-tab ${isSelected ? 'selected' : ''}`}>
-      <span className="tab-title">{ index }</span>
-      <IconButton className="close-btn">
-        <CloseIcon />
-      </IconButton>
-    </li>
+    <Link to={{ search: `?tab=${index}` }} className={`editor-tab ${isSelected ? 'selected' : ''}`}>
+      <span className="tab-title">{index}</span>
+      {index === 0 ? null : (
+        <IconButton className="close-btn" onClick={(e) => deleteTab(e)}>
+          <CloseIcon />
+        </IconButton>
+      )}
+    </Link>
   );
 }
